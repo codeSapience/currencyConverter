e599eb9a4c7eecd4df820b16fb8689aad1c203bf
akintade <brittoakintade@gmail.com>
2018-06-29 17:42:26 +0100

    Twentieth  commit (Adding a working SW to currency converter to work offline)

--- currency_conversion.js
+++ currency_conversion.js
@@ -5,6 +5,16 @@ Year: 2018;
 Description: A Vanilla-js library to convert amount in one currency to another.
 
 */
+if ('serviceWorker' in navigator){
+  navigator.serviceWorker.register('sw.js')
+  .then(function(reg) {
+    // Message for successful sw registration
+    console.log('Successful! Currency Converter SW has been registered.');
+  }).catch(function(error) {
+    // Message for failed sw registration
+    console.log(`UNSUCCESSFUL! Currency Converter SW registration failed. ${error}`);
+  });
+}
 
         let currencies_url;
 
@@ -16,7 +26,7 @@ Description: A Vanilla-js library to convert amount in one currency to another.
         .then(() => {
             // console.log(JSobj);
             const results = JSobj.results;
-            let resultsKeys = Object.keys(results);
+            let resultsKeys = Object.keys(results).sort();
             // let resultsValues = Object.values(results);
             let dropdownText = "";
             for (item of resultsKeys){
