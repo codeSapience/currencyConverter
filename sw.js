51f2a7726253b91397267e0ef2bf22fcab470534
brittoakintade@gmail.com <brittoakintade@gmail.com>
2018-07-31 11:17:03 +0100

    Modify sw.js to use relative path in caching index.html

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const CACHE_NAME = 'cache-v1';  
 const urlsToCache = [  
-  '/index.html',
+  '/',
   // './currency_conversion.js',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css',
   'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css',
