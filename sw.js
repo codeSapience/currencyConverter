4ed63ca6b33ea3378d8e2c49403192e6aafddef8
brittoakintade@gmail.com <brittoakintade@gmail.com>
2018-07-31 11:01:25 +0100

    Edit sw.js to explicitly include index.html in cache

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const CACHE_NAME = 'cache-v1';  
 const urlsToCache = [  
-  '/',
+  '/index.html',
   // './currency_conversion.js',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css',
   'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css',
@@ -13,7 +13,6 @@ self.addEventListener('install', event => {
     });
 });
 
-
 // self.addEventListener('fetch', event => {  
 //   const { request } = event;
 
@@ -25,3 +24,4 @@ self.addEventListener('install', event => {
 //     .then(cache => fetch(request)
 //       .then(response => cache.put(request, response))));
 // });
+
